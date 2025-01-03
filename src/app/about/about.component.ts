391a15dfae47016af9d6f5d3cbc35fe8460ad720
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-01-04 00:00:46 +0200

    style skills section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SkillsComponent } from "../skills/skills.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [SkillsComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
