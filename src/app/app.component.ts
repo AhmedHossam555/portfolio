db14e2638cc370e982653d755725425ba29db607
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-01-04 02:16:52 +0200

    footer section style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, AboutComponent, ProjectsComponent, ContactComponent],
+  imports: [HomeComponent, AboutComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
