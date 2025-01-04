8ab4816315a558b5e4242d96347d9e4be2cc5454
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2025-01-04 02:01:50 +0200

    contact section style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { ProjectsComponent } from "./projects/projects.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, AboutComponent, ProjectsComponent],
+  imports: [HomeComponent, AboutComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
